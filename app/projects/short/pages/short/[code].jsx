a866a8403f18a7447d950c943b22063dc5a13a35
ApteryxXYZ <apteryx.xyz@gmail.com>
2022-03-20 20:58:44 +1300

    🐛 Fix clickShort method

--- app/projects/short/pages/short/[code].jsx
+++ app/projects/short/pages/short/[code].jsx
@@ -9,7 +9,7 @@ export async function getServerSideProps(context) {
     const code = context.query.code;
     const short = await getShort({ code });
     if (!short) return { notFound: true };
-    await clickShort({ where: { code } });
+    await clickShort({ code });
     return {
         redirect: {
             destination: short.target,
